9803e5a4131141132dd8a94ab41b5471fd403999
caiquepark <caiquepark@hotmail.com>
2021-11-26 19:13:03 -0300

    commit inicial modulos main e auth

---
+++ projeto-pratico/src/app/mauth/mauth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MauthRoutingModule } from './mauth-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MauthRoutingModule
+  ]
+})
+export class MauthModule { }
