f39f9c54c73aa96f8e1959dc6435973627fdb923
caiquepark <caiquepark@hotmail.com>
2021-11-29 23:02:35 -0300

    injetanco canActivate e implementando regra de segurança no firebase

--- projeto-pratico/src/app/app-routing.module.ts
+++ projeto-pratico/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuardService } from './mauth/service/auth-guard.service';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/main/people'},
-  {path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule)},
+  {path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule), canActivate: [AuthGuardService]},
   {path: '**', component: NotFoundComponent}
 ];
 
